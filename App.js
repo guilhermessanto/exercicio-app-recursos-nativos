5935381a83da1145de9923f7ffb25b84719dd92a
Guilherme Santana <g.santossantana22@gmail.com>
2023-01-20 11:50:21 -0300

    mapa finalizado

--- App.js
+++ App.js
@@ -1,10 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TextInput,
+  Pressable,
+} from "react-native";
+
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
 
 export default function App() {
+  const [texto, onChangeText] = useState("Titulo da foto/local");
+  const [minhaLocalizacao, setMinhaLocalizacao] = useState(null);
+
+  useEffect(() => {
+    async function obterLocalizacao() {
+      const { status } = Location.requestForegroundPermissionsAsync();
+
+      let localizacaoAtual = await Location.getCurrentPositionAsync({});
+      setMinhaLocalizacao(localizacaoAtual);
+    }
+    obterLocalizacao();
+  }, []);
+
+  const regiaoInicial = {
+    latitude: -23.533773,
+    longitude: -46.65529,
+    latitudeDelta: 10,
+    longitudeDelta: 10,
+  };
+  const [localizacao, setLocalizacao] = useState();
+  const novaLocalizacao = (event) => {
+    setLocalizacao({
+      latitude: minhaLocalizacao.coords.latitude,
+      longitude: minhaLocalizacao.coords.longitude,
+      latitudeDelta: 0.0052,
+      longitudeDelta: 0.0012,
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <SafeAreaView>
+        <StatusBar animated={true} backgroundColor="black" />
+        <Text style={styles.texto}>App 1 - Fotos de lugares visitados</Text>
+        <View style={styles.caixa}>
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeText}
+            value={texto}
+          />
+          <View style={styles.viewMapa}></View>
+          <Pressable style={styles.botao}>
+            <Text style={styles.textoBotao}>Tirar foto</Text>
+          </Pressable>
+        </View>
+        <View style={styles.caixa}>
+          <View style={styles.viewMapa}>
+            <MapView
+              style={styles.map}
+              region={localizacao ?? regiaoInicial}
+              liteMode={false}
+            >
+              {localizacao && (
+                <Marker coordinate={localizacao} title="Titulo" draggable />
+              )}
+            </MapView>
+          </View>
+          <Pressable style={styles.botao} onPress={novaLocalizacao}>
+            <Text style={styles.textoBotao}>localizar no mapa</Text>
+          </Pressable>
+        </View>
+      </SafeAreaView>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +83,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  texto: {
+    textAlign: "center",
+    fontSize: 18,
+  },
+  caixa: {
+    width: 400,
+    alignItems: "center",
+  },
+  input: {
+    height: 60,
+    width: 350,
+    margin: 30,
+    borderWidth: 1,
+    padding: 10,
+  },
+  viewMapa: {
+    height: 200,
+    width: 350,
+    backgroundColor: "gray",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+  botao: {
+    height: 40,
+    width: 350,
+    marginVertical: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 4,
+    backgroundColor: "gray",
+  },
+  textoBotao: {
+    fontSize: 20,
   },
 });
